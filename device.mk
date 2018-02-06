d94975dbfe2e3f50cb2dd02ef134cb7fd1661938
AndikaBayuS <andika.bayu.s2001@gmail.com>
2018-02-06 10:38:37 +0700

    Update something

--- device.mk
+++ device.mk
@@ -355,5 +355,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
 
+# IMS
+PRODUCT_PACKAGES += \
+      telephony-ext
+
 PRODUCT_BOOT_JARS += \
     telephony-ext
